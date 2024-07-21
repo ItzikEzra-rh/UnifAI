3518f1c34c635e0d6f82c51629b2386a1387d2d5
rashti <nir.rashti@gmail.com>
2024-07-21 10:09:15 +0000

    Backend Initial template

--- backend/TestGenie/endpoints/asc/__init__.py
+++
@@ -1,7 +0,0 @@
-from .asc import asc_bp
-from .sample import asc_samples_bp
-
-asc_blueprints = [
-    {"bp": asc_bp, "parent": 'asc', "route": ''},
-    {"bp": asc_samples_bp, "parent": 'asc', "route": 'sample'}
-]
--- backend/TestGenie/endpoints/lucid/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lucid import lucid_bp
-
-lucid_blueprints = [
-    {"bp": lucid_bp, "parent": 'lucid', "route": ''}
-]
