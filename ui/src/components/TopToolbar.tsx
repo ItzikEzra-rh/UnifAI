60f6f8a48d171f9941ed2a38b17fabdbc0040967
rashti <nir.rashti@gmail.com>
2024-07-22 11:55:29 +0000

    Tool Bar adjustments

--- ui/src/components/TopToolbar.tsx
+++ ui/src/components/TopToolbar.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../styles.css';
 import RedHatLogo from '../assets/RedhatLogo.png';
+import { Button } from '@mui/material';
 
 const TopToolbar: React.FC = () => {
   return (
     <div className="top-toolbar">
       <div className="logo">
         <img src={RedHatLogo} alt="Logo" className="logo-image" />
+      </div>
+      <div className="toolbar-buttons">
+        <Button variant="outlined" className="custom-button">Log In</Button>
+        <Button variant="outlined" className="custom-button">Support</Button>
       </div>
     </div>
   );
