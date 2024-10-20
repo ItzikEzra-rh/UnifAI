439267ae0cc8cdc1e0bc566f86d874e38ca836be
Eric Guanghui Zhou <eric.guanghui.zhou@nokia.com>
2024-10-20 09:49:46 +0800

    containerization the parser

--- data-pre/grammar.js
+++ data-pre/grammar.js
@@ -1,13 +1,3 @@
-
-// const SECTION_NAMES = [
-//   "Settings",
-//   "Variables",
-//   "Test Cases",
-//   "Tasks",
-//   "Keywords",
-//   "Comments",
-// ]
-
 const SETTINGS_KEYWORDS = [
   "Library",
   "Resource",
@@ -37,29 +27,19 @@ function caseInsensitive(keyword) {
   );
 }
 
-/**
- * Shortcut for defining a section header
- */
 function section_header($, name) {
   return seq(
     alias(
-      token(
-        seq("***", optional(" "), caseInsensitive(name), optional(" "), "***")
-      ),
+      token(seq("***", optional(" "), caseInsensitive(name), optional(" "), "***")),
       $.section_header
     ),
     optional(alias(/[^\r\n]+/, $.extra_text)),
-    $._line_break,
-  )
+    $._line_break
+  );
 }
 
-/**
- * Shortcut for defining a setting token
- */
 function setting(name) {
-  return token(
-    seq("[", optional(" "), caseInsensitive(name), optional(" "), "]")
-  )
+  return token(seq("[", optional(" "), caseInsensitive(name), optional(" "), "]"));
 }
 
 module.exports = grammar({
@@ -70,7 +50,7 @@ module.exports = grammar({
   conflicts: $ => [
     [$.keyword_invocation],
     [$.variable_assignment],
-    [$.arguments],   
+    [$.arguments],
     [$.continuation],
     [$.block],
     [$.except_statement],
@@ -91,60 +71,54 @@ module.exports = grammar({
       $.test_cases_section,
     ),
 
-    //
     // Settings section
-    //
-
-    settings_section: $ => seq(
-      section_header($, "Settings"),
-      repeat(choice(
-        $.setting_statement,
-        $._empty_line,
-      )),
-    ),
+        settings_section: $ => seq(
+          section_header($, "Settings"),
+          repeat(choice(
+                $.setting_statement,
+                $.ignored_setting,  // Ignore any setting that starts with Suite or Test
+                $._empty_line,
+          )),
+        ),
     setting_statement: $ => seq(
       choice(...SETTINGS_KEYWORDS.map(caseInsensitive)),
       $.arguments,
       $._line_break,
     ),
-
-    //
+        ignored_setting: $ => seq(
+          choice(
+                /Suite\s+\S+/,
+                /Test\s+\S+/
+          ),
+          /.*/,  // Skip the rest of the line
+          $._line_break
+        ),
     // Variables section
-    //
-
     variables_section: $ => seq(
       section_header($, "Variables"),
-      repeat(choice(
-        $.variable_definition,
-        $._empty_line,
-      )),
+      repeat(choice($.variable_definition, $._empty_line)),
     ),
     variable_definition: $ => seq(
-      seq("${", $.variable_name, "}"),
-      optional(
-        choice("=", " =")
+      choice(
+        seq("${", $.variable_name, "}"),
+        seq("@{", $.variable_name, "}"),
+        seq("&{", $.variable_name, "}")
       ),
-      $.arguments,
+      optional(choice("=", " =")),
+      optional($.arguments),
       $._line_break,
     ),
 
-    //
     // Keywords section
-    //
-
     keywords_section: $ => seq(
       section_header($, "Keywords"),
-      repeat(choice(
-        $.keyword_definition,
-        $._empty_line,
-      )),
+      repeat(choice($.keyword_definition, $._empty_line)),
     ),
     keyword_definition: $ => seq(
       alias($.text_chunk, $.name),
       $._line_break,
       alias($.keyword_definition_body, $.body),
     ),
-    // prec.right is needed to capture $._empty_line more strongly than $.keywords_section
     keyword_definition_body: $ => prec.right(repeat1(
       choice(
         seq($._indentation, $.keyword_setting, $._line_break),
@@ -152,47 +126,30 @@ module.exports = grammar({
         $._empty_line,
       )
     )),
-    // Ref: http://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#keyword-section-1
-    keyword_setting: $ => seq(
+    keyword_setting: $ => prec.right(seq(
       choice(
-        setting("Documentation"),
-        setting("Tags"),
-        setting("Arguments"),
-        setting("Return"),
-        setting("Teardown"),
-        setting("Timeout"),
+        seq(setting("Documentation"), optional($.arguments), optional($._line_break)),
+        seq(setting("Tags"), $.arguments, $._line_break),
+        seq(setting("Arguments"), optional($.arguments), $._line_break),
+        seq(setting("Return"), $.arguments, $._line_break),
+        seq(setting("Teardown"), $.arguments, $._line_break),
+        seq(setting("Timeout"), $.arguments, $._line_break),
       ),
-      $.arguments,
-    ),
+      optional($.arguments),
+    )),
 
-    //
     // Test cases / Tasks section
-    //
-
     test_cases_section: $ => seq(
       section_header($, "Test Cases"),
-      repeat(choice(
-        $.test_case_definition,
-        $._empty_line,
-      )),
+      repeat(choice($.test_case_definition, $._empty_line)),
     ),
     test_case_definition: $ => seq(
       alias($.text_chunk, $.name),
       choice(
-        // Data-driven tests have arguments
-        seq(
-          alias($.arguments_without_continuation, $.arguments),
-          $._line_break,
-        ),
-
-        // Regular tests have bodies
-        seq(
-          $._line_break,
-          alias($.test_case_definition_body, $.body),
-        )
+        seq(alias($.arguments_without_continuation, $.arguments), $._line_break),
+        seq($._line_break, alias($.test_case_definition_body, $.body))
       )
     ),
-    // prec.right is needed to capture $._empty_line more strongly than $.test_case_section
     test_case_definition_body: $ => prec.right(repeat1(
       choice(
         seq($._indentation, $.test_case_setting, $._line_break),
@@ -200,7 +157,6 @@ module.exports = grammar({
         $._empty_line,
       )
     )),
-    // Ref: http://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#test-case-section
     test_case_setting: $ => seq(
       choice(
         setting("Documentation"),
@@ -213,13 +169,7 @@ module.exports = grammar({
       $.arguments,
     ),
 
-    //
     // Statements
-    //
-    // These are the lines that appear within test cases, tasks and user
-    // defined keywords
-    //
-
     statement: $ => seq(
       choice(
         $.variable_assignment,
@@ -235,66 +185,51 @@ module.exports = grammar({
       ),
     ),
 
+    // Return statement
     return_statement: $ => prec.right(seq(
       "RETURN",
-      optional(
-        seq(
-          $._separator,
-          alias($.argument, $.return_value),
-        )
-      )
+      optional(seq($._separator, alias($.argument, $.return_value)))
     )),
 
-    variable_assignment: $ => seq(
-      seq("${", $.variable_name, "}"),
-      optional(
-        choice("=", " =")
-      ),
-      optional($.arguments),
-    ),
+        variable_assignment: $ => seq(
+          choice(
+                seq("${", $.variable_name, "}"),  // Scalar variable
+                seq("@{", $.variable_name, "}"),  // List variable
+                seq("&{", $.variable_name, "}")   // Dictionary variable
+          ),
+          optional(choice("=", " =")),  // Allow both `=` and `=` assignment operators
+          optional($.arguments),  // Arguments after the assignment
+        ),
 
     keyword_invocation: $ => seq(
       alias($.text_chunk, $.keyword),
       optional($.arguments),
     ),
 
+    // If / Else / Try statements
     if_statement: $ => seq(
       "IF",
       $._separator,
       field("condition", $.argument),
       $._line_break,
-
       optional(field("consequence", $.block)),
-
       repeat(field("alternative", $.elseif_statement)),
-
       optional(field("alternative", $.else_statement)),
-
       seq($._indentation, "END"),
     ),
 
-    elseif_statement: $ => prec.dynamic(100, seq(   // Conflicts with parent structures because of $._line_break
-      $._indentation,
-      "ELSE IF",
-      $._separator,
-      field("condition", $.argument),
-      $._line_break,
-      field("consequence", $.block),
+    elseif_statement: $ => prec.dynamic(100, seq(
+      $._indentation, "ELSE IF", $._separator, field("condition", $.argument), $._line_break, field("consequence", $.block)
     )),
 
-    else_statement: $ => prec.dynamic(100, seq(   // Conflicts with parent structures because of $._line_break
-      $._indentation,
-      "ELSE",
-      $._line_break,
-      field("consequence", $.block),
-    )),
+    else_statement: $ => prec.dynamic(100, seq($._indentation, "ELSE", $._line_break, field("consequence", $.block))),
 
     inline_if_statement: $ => seq(
       "IF",
       $._separator,
       $.argument,
       optional( seq(
-	      $._line_break,
+              $._line_break,
               $.indented_ellipses,
       )),
       $._separator,
@@ -303,35 +238,12 @@ module.exports = grammar({
       optional(seq($._separator, $.inline_else_statement)),
     ),
 
-    block: $ => repeat1(
-      choice(
-        $._empty_line,
-        seq($._indentation, $.statement, $._line_break),
-      )
-    ),
+    block: $ => repeat1(choice($._empty_line, seq($._indentation, $.statement, $._line_break))),
 
-    inline_elseif_statement: $ => prec.dynamic(100, seq(   // Conflicts with $.arguments
-      "ELSE IF",
-      $._separator,
-      $.argument,
-      $._separator,
-      $.inline_statement,
-    )),
-    
-    inline_else_statement: $ => prec.dynamic(100, seq(   // Conflicts with $.arguments
-      "ELSE",
-      $._separator,
-      $.inline_statement,
-    )),
+    inline_elseif_statement: $ => prec.dynamic(100, seq("ELSE IF", $._separator, $.argument, $._separator, $.inline_statement)),
+    inline_else_statement: $ => prec.dynamic(100, seq("ELSE", $._separator, $.inline_statement)),
 
-    // Any statement that can occur inside an inline if/else statement
-    inline_statement: $ => seq(
-      choice(
-        $.variable_assignment,
-        $.keyword_invocation,
-        $.return_statement,
-      ),
-    ),
+    inline_statement: $ => seq(choice($.variable_assignment, $.keyword_invocation, $.return_statement)),
 
     try_statement: $ => seq(
       "TRY",
@@ -340,43 +252,25 @@ module.exports = grammar({
       repeat($.except_statement),
       prec.dynamic(200, optional($.else_statement)),
       optional($.finally_statement),
-      $._indentation,
-      "END",
+      $._indentation, "END"
     ),
 
-    except_statement: $ => prec.dynamic(100, seq(
-      $._indentation,
-      "EXCEPT",
-      optional($.arguments),
-      $._line_break,
-      optional($.block),
-    )),
+    except_statement: $ => prec.dynamic(100, seq($._indentation, "EXCEPT", optional($.arguments), $._line_break, optional($.block))),
 
-    finally_statement: $ => prec.dynamic(100, seq(
-      $._indentation,
-      "FINALLY",
-      $._line_break,
-      optional($.block),
-    )),
+    finally_statement: $ => prec.dynamic(100, seq($._indentation, "FINALLY", $._line_break, optional($.block))),
 
+    // While / For statements
     while_statement: $ => seq(
       "WHILE",
       field("condition", alias($.arguments_without_continuation, $.arguments)),
       $._line_break,
       field("body", optional($.block)),
-      $._indentation,
-      "END",
+      $._indentation, "END"
     ),
 
     for_statement: $ => seq(
       "FOR",
-      field("left", alias(
-        repeat1(seq(
-          $._separator,
-          $.scalar_variable,
-        )),
-        $.variable_list),
-      ),
+      field("left", alias(repeat1(seq($._separator, $.scalar_variable)), $.variable_list)),
       $._separator,
       field("right", choice(
         alias($._for_in, $.in),
@@ -386,73 +280,34 @@ module.exports = grammar({
       )),
       $._line_break,
       field("body", $.block),
-      $._indentation,
-      "END"
+      $._indentation, "END"
     ),
 
-    _for_in: $ => seq(
-      seq("IN", $.arguments),
-    ),
+    _for_in: $ => seq(seq("IN", $.arguments)),
 
     _for_in_range: $ => seq(
       "IN RANGE",
       $._separator,
       $.argument,
-      optional(seq(
-        $._separator,
-        $.argument,
-        optional(seq(
-          $._separator,
-          $.argument,
-        ))
-      ))
+      optional(seq($._separator, $.argument, optional(seq($._separator, $.argument))))
     ),
 
-    _for_in_enumerate: $ => seq(
-      seq("IN ENUMERATE", $.arguments),
-    ),
+    _for_in_enumerate: $ => seq("IN ENUMERATE", $.arguments),
 
-    _for_in_zip: $ => seq(
-      seq("IN ZIP", $.arguments),
-    ),
+    _for_in_zip: $ => seq("IN ZIP", $.arguments),
 
     continue_statement: $ => "CONTINUE",
-
     break_statement: $ => "BREAK",
 
-    //
-    // Reusable rules
-    //
-
-    // NB: Arguments includes the preceding separator
+    // Arguments
     arguments: $ => seq(
-      choice(
-        seq(
-          repeat1(seq(
-            $._separator,
-            $.argument,
-          )),
-          repeat($.continuation),
-        ),
-        repeat1($.continuation)
-      )
+      repeat1(seq($._separator, choice($.argument, seq($.scalar_variable, "=", $.argument)))),
+      repeat($.continuation)
     ),
 
-    arguments_without_continuation: $ => repeat1(seq(
-      $._separator,
-      $.argument,
-    )),
+    arguments_without_continuation: $ => repeat1(seq($._separator, $.argument)),
 
-    // Conflicts with parent structures because of $._line_break
-    continuation: $ => prec.dynamic(100, seq(
-      $._line_break,
-      optional($._indentation),
-      $.ellipses,
-      repeat(seq(
-        $._separator,
-        $.argument,
-      )),
-    )),
+    continuation: $ => prec.dynamic(100, seq($._line_break, optional($._indentation), $.ellipses, repeat(seq($._separator, $.argument)))),
 
     ellipses: $ => "...",
     indented_ellipses: $ => token(seq(
@@ -461,83 +316,30 @@ module.exports = grammar({
       "..."
     )),
 
+
     argument: $ => seq(
-      choice(
-        $.text_chunk,
-        $.scalar_variable,
-        $.list_variable,
-        $.dictionary_variable,
-        $.inline_python_expression,
-      ),
-      repeat(seq(
-        optional(" "),
-        choice(
-          $.text_chunk,
-          $.scalar_variable,
-          $.inline_python_expression,
-        )
-      ))
+      choice($.text_chunk, $.scalar_variable, $.list_variable, $.dictionary_variable, $.inline_python_expression),
+      repeat(seq(optional(" "), choice($.text_chunk, $.scalar_variable, $.inline_python_expression)))
     ),
 
-    scalar_variable: $ => seq(
-      "${",
-      optional(" "),
-      $.variable_name,
-      optional(" "),
-      "}"
-    ),
+    scalar_variable: $ => seq("${", optional(" "), $.variable_name, optional(" "), "}"),
+    list_variable: $ => seq("@{", optional(" "), $.variable_name, optional(" "), "}"),
+    dictionary_variable: $ => seq("&{", optional(" "), $.variable_name, optional(" "), "}"),
 
-    list_variable: $ => seq(
-      "@{",
-      optional(" "),
-      $.variable_name,
-      optional(" "),
-      "}"
-    ),
-
-    dictionary_variable: $ => seq(
-      "&{",
-      optional(" "),
-      $.variable_name,
-      optional(" "),
-      "}"
-    ),
-
-    inline_python_expression: $ => prec.left(seq(
-      "${{",
-      alias(repeat(choice("}", /[^\r\n}]+/)), $.python_expression),
-      "}}",
-    )),
+    inline_python_expression: $ => prec.left(seq("${{", alias(repeat(choice("}", /[^\r\n}]+/)), $.python_expression), "}}")),
 
     variable_name: $ => /[^{}]+/,
 
     text_chunk: $ => token(seq(
-      choice(
-        /[^\s$@&{#]/,   // Can't start with a #, since that would be a comment
-        /[$@&][^{]/,
-        /[^$@&]\{/,
-      ),
-      repeat(choice(
-        /[^\s$@&{]/,
-        /[$@&][^{]/,
-        /[^$@&]\{/,
-      )),
-      repeat(seq(
-        " ",
-        repeat1(choice(
-          /[^\s$@&{]/,
-          /[$@&][^{]/,
-          /[^$@&]\{/,
-        )),
-      )),
+      choice(/[^\s$@&{#]/, /[$@&][^{]/, /[^$@&]\{/),
+      repeat(choice(/[^\s$@&{]/, /[$@&][^{]/, /[^$@&]\{/)),
+      repeat(seq(" ", repeat1(choice(/[^\s$@&{]/, /[$@&][^{]/, /[^$@&]\{/))))
     )),
 
     comment: $ => token(seq(optional(/[ \t]+/), "#", /[^\n]*/)),
 
     _separator: $ => token(seq(/[ ]{2}|\t/, optional(/[ \t]+/))),
     _indentation: $ => $._separator,
-
-    _whitespace: $ => /[ \t]+/,
 
     _line_break: $ => /\s*(\r?\n)/,
 
---
+++ data-pre/scan.robot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Directory to search in
+directory="$1"
+
+# Check if the directory is provided
+if [ -z "$directory" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+# Find all .robot files in the directory
+robot_files=($(find "$directory" -type f -name "*.robot"))
+
+# Get the number of .robot files
+file_count=${#robot_files[@]}
+
+# Provide the number of files
+echo "Found $file_count .robot files in the directory: $directory"
+
+# Exit if no files found
+if [ "$file_count" -eq 0 ]; then
+  echo "No .robot files found."
+  exit 0
+fi
+
+# Loop through each .robot file and run the tt command
+for file in "${robot_files[@]}"; do
+  echo "Processing file: $file"
+  tree-sitter parse  -sq "$file"
+  if [ $? -ne 0 ]; then
+    echo "Error running 'tt' on $file"
+  fi
+done
+
