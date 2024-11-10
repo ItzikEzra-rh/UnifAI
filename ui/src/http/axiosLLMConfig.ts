15b8ed77f8679fbe1709837c852ca03352ea6d4d
nrashti <nrashti@redhat.com>
2024-11-10 08:05:00 -0500

    GUI | Change LLM config

--- ui/src/http/axiosLLMConfig.ts
+++ ui/src/http/axiosLLMConfig.ts
@@ -4,7 +4,7 @@ export const AXIOS_LLM_IP= 'http://instructlab.sdn5r.sandbox429.opentlc.com:443'
 
 const axiosInstance = axios.create({
   baseURL: 'http://instructlab.sdn5r.sandbox429.opentlc.com:443',
-  timeout: 10000, // 10 seconds
+  timeout: 100000, // 10 seconds
 });
 
 export default axiosInstance;
