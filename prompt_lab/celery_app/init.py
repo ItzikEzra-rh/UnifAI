7c40fb43b10c623a41b369bf85e87cbde7468d5c
Odai Odeh <oodeh@redhat.com>
2024-11-19 22:54:44 +0200

    adding template, not tested yet

---
+++ prompt_lab/celery_app/init.py
@@ -0,0 +1,22 @@
+"""
+Module for importing non-configured flask extensions
+"""
+from celery_app import Celery
+from config.configParams import config_params
+
+
+def make_celery():
+    """Initialize and configure Celery.
+    run celery using:
+    celery -A celery_app.init worker --loglevel=info
+    """
+    celery_app = Celery('celery_promp_lab',
+                        broker=config_params.RABBITMQ_URL,
+                        backend='mongodb://your-mongodb-url/your-collection',
+                        include=['celery_app.tasks'])
+    return celery_app
+
+
+celery = make_celery()
+
+
