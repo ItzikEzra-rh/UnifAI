7da23047774b31aa8a902aeb07ef513a4280e33d
Odai Odeh <oodeh@redhat.com>
2024-11-22 11:45:18 +0200

    make one point of celery object

--- reviewer/utils/celery/celery.py
+++ reviewer/utils/celery/celery.py
@@ -1,22 +1,8 @@
 from utils.util import get_rabbitmq_url
-from celery import Celery
-
-_celery_client = Celery('celery',
-                        broker=get_rabbitmq_url(),
-                        BROKER_USER='genie',
-                        BROKER_PASSWORD='genie123')
-
-
-def celery_client():
-    """
-    get the db instance
-    """
-    global _celery_client
-    return _celery_client
+from celery_app.init import celery
 
 
 def send_task(task_name, celery_queue, **kwargs):
-    client = celery_client()
-    client.send_task(f"celery_app.tasks.{task_name}",
+    celery.send_task(f"celery_app.tasks.{task_name}",
                      queue=celery_queue,
                      kwargs=kwargs)
