d0f30fdec0f10ce90979601ed8cc131fde3a8317
nrashti <nrashti@redhat.com>
2024-11-27 03:40:14 -0500

    Change file names without g_eval prefix

--- reviewer/g_eval/qa_scoring_system.py
+++ reviewer/g_eval/qa_scoring_system.py
@@ -7,8 +7,8 @@ from transformers import AutoTokenizer
 
 from logger import logger
 from g_eval.config import EvalMetric, Config, GEvalConfig
-from g_eval.g_eval_prompt_formatter import GEvalPromptFormatter
-from g_eval.g_eval_scorer import GEvalScorer
+from reviewer.g_eval.prompt_formatter import GEvalPromptFormatter
+from reviewer.g_eval.scorer import GEvalScorer
 from utils.celery.celery import send_task
 
 class GEvalQASystem:
