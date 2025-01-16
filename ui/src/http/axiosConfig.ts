9d30686399132f72cedd34a21015c329b1ff89e9
Eric Guanghui Zhou <eric.guanghui.zhou@nokia.com>
2025-01-16 21:02:56 +0800

    use the service name for BE and LLM-BE

--- ui/src/http/axiosConfig.ts
+++ ui/src/http/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:13456',
+  //baseURL: 'http://127.0.0.1:13456',
+  baseURL: 'http://genie-be:8080',
   timeout: 20000, // 20 seconds
 });
 
