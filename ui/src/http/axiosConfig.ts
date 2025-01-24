55e43167ecf6143106d7e8dbc48404c541b580d1
Eric Zhou <gzhou@redhat.com>
2025-01-24 14:14:42 +0000

    change the axios baseUrl to /

--- ui/src/http/axiosConfig.ts
+++ ui/src/http/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://127.0.0.1:13456',
-  baseURL: 'http://genie-be:8080',
+  baseURL: '/',
   timeout: 20000, // 20 seconds
 });
 
