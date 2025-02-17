744c8d52f31c656ee13c2c3d2c7bbb10fabd67dd
MayaCrmi <your_email@example.com>
2025-02-17 05:01:01 -0500

    back to original config

--- ui/src/http/axiosConfig.ts
+++ ui/src/http/axiosConfig.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:13456',
-  // baseURL: '/',
+  // baseURL: 'http://127.0.0.1:13456',
+  baseURL: '/',
   timeout: 20000, // 20 seconds
 });
 
