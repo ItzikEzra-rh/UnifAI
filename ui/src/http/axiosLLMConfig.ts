c61d293f7eebf33b7018fc7888810f8218431d54
MayaCrmi <your_email@example.com>
2025-02-17 03:54:55 -0500

    first config

--- ui/src/http/axiosLLMConfig.ts
+++ ui/src/http/axiosLLMConfig.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-//export const AXIOS_LLM_IP= 'http://instructlab.jf42w.sandbox1115.opentlc.com:443'
-export const AXIOS_LLM_IP= '/'
+export const AXIOS_LLM_IP= 'http://instructlab.dlvxc.sandbox1300.opentlc.com:443'
+// export const AXIOS_LLM_IP= '/'
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://instructlab.jf42w.sandbox1115.opentlc.com:443',
-  baseURL: '/',
+  baseURL: 'http://instructlab.dlvxc.sandbox1300.opentlc.com:443',
+  // baseURL: '/',
   timeout: 300000, // 300 seconds
 });
 
