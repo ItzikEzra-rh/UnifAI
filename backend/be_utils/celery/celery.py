bf56aa5020629a0993831bcf9f9a1fe050a13e69
Odai Odeh <oodeh@redhat.com>
2025-05-28 09:44:50 +0300

    REMOVE genie files

--- backend/be_utils/celery/celery.py
+++
@@ -1,8 +0,0 @@
-
-from backend.celery_app.init import CeleryApp
-
-
-def send_task(task_name, celery_queue, **kwargs):
-    CeleryApp().app.send_task(task_name,
-                              kwargs=kwargs,
-                              queue=celery_queue)
