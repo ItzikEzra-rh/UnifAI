4060487cd4142f0fbd68976255628fc284fcadae
Yosi Habushi <yhabushi@redhat.com>
2025-06-05 23:31:14 +0300

    unified celery with multiple queues

--- helm/archive/entrypoint.sh
+++ helm/archive/entrypoint.sh
@@ -14,15 +14,21 @@ case "$ROLE" in
     exec python app.py
     ;;
     
-  celery)
+  slack-celery)
     echo "🔧 Starting Slack Celery worker with tasks concurrently : $CELERY_WORKER"
     . ~/backend/venv/bin/activate
-    exec celery -A celery_app.init worker -c $CELERY_WORKER --loglevel=info -Q $CELERY_QUEUES -n data_sources
+    exec celery -A celery_app.init worker -c $CELERY_WORKER --loglevel=info -Q slack_queue -n data_sources
+    ;;
+
+  docs-celery)
+    echo "⏰ Starting Docs Celery worker with tasks concurrently : $CELERY_WORKER"
+    . ~/backend/venv/bin/activate
+    exec celery -A celery_app.init worker -c $CELERY_WORKER --loglevel=info -Q docs_queue -n data_sources
     ;;
 
   *)
     echo "❌ ERROR: Unknown ROLE \"$ROLE\""
-    echo "Valid roles are: flask, celery"
+    echo "Valid roles are: flask, slack-celery, doc-celery"
     exit 1
     ;;
 esac
