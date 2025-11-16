59608f7fcfc9aac95d7b73d8b97f79e38c69689c
labuyous <labuyous@redhat.com>
2025-11-16 06:08:05 -0500

    cr

--- DataPipelineHub/backend/validator/doc_validators/__init__.py
+++ DataPipelineHub/backend/validator/doc_validators/__init__.py
@@ -1,35 +1,7 @@
-from typing import Optional, Dict, Any, List, Tuple
-from common.interfaces import DataSourceValidator, ValidationIssue
-from services.documents.duplicate_checker import DocumentDuplicateChecker
-from utils.storage.mongo.mongo_helpers import get_mongo_storage
+from .duplicate_validator import DuplicateValidator
+from .doc_validators import DocValidators
 
-class DuplicateValidator(DataSourceValidator):
-    name = "DuplicateValidator"
-    error_message = "This file appears to be a duplicate from an existing file and was not added. File: {source_name}"
-    error_message_key = "File duplicated error"
-
-    def __init__(self) -> None:
-        # The validator owns and initializes its dependency
-        self.duplicate_checker = DocumentDuplicateChecker(get_mongo_storage())
-
-    def validate(self, **kwargs: Any) -> Tuple[bool, Optional[ValidationIssue]]:
-        try:
-            if self.duplicate_checker.is_duplicate(kwargs):
-                return False, self.build_issue(
-                    self.error_message.format(source_name=kwargs.get("source_name"))
-                )
-        except Exception:
-            return True, None
-
-        return True, None
-
-
-class DocValidators:
-    """
-    Constructs the document validators pipeline.
-    """
-    def create_validators(self) -> List[DataSourceValidator]:
-        return [
-            DuplicateValidator(),
-        ]
-
+__all__ = [
+	"DuplicateValidator",
+	"DocValidators",
+]
