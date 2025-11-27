dc0ceed4e072257c0988aae10f6d1438284fe9a9
MayaCrmi <your_email@example.com>
2025-11-27 07:38:53 -0500

    after PR

--- ui/client/src/components/agentic-ai/AvailableFlows.tsx
+++ ui/client/src/components/agentic-ai/AvailableFlows.tsx
@@ -14,6 +14,7 @@ import {
 } from "lucide-react";
 import { useAuth } from "@/contexts/AuthContext";
 import { useShared } from "@/contexts/SharedContext";
+import axios from '../../http/axiosAgentConfig'
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
